2a8339e0dac4b5835c067ef447b76807bb600793
Cecilia (Chae Hyun) Lee <chaelee@email.unc.edu>
2022-03-24 09:49:44 -0400

    help msg

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 import { createRequire } from "module";
+
 const require = createRequire(import.meta.url);
 const express = require("express");
 const app = express();
 const minimist = require("minimist");
+const morgan = require("morgan");
 
 const args = minimist(process.argv.slice(2));
 const port = args["port"];
+const database = require("./database.js");
 const aPort = port || process.env.PORT || 3000;
+const { exit } = require('process');
+
+
+if (args['help']) {
+  console.log(`server.js [options]
+    --port	Set the port number for the server to listen on. Must be an integer
+                between 1 and 65535.
+  
+    --debug	If set to \`true\`, creates endpoints /app/log/access/ which returns
+                a JSON access log from the database and /app/error which throws 
+                an error with the message "Error test successful." Defaults to 
+                \`false\`.
+  
+    --log	If set to false, no log files are written. Defaults to true.
+                Logs are always written to database.
+  
+    --help	Return this message and exit.`)
+  exit(EXIT_SUCCESS)
+}
 
 // coin functions
 function coinFlip() {
