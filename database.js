263b9be277ca0c3c0e613f54f43a9ed8f9030838
Cecilia (Chae Hyun) Lee <chaelee@email.unc.edu>
2022-04-07 11:20:54 -0400

    create db

---
+++ database.js
@@ -0,0 +1,30 @@
+const db = require('better-sqlite3')
+const Database = require('better-sqlite3')
+
+const datab = new Database('user.db')
+const stmt = db.prepare(`
+    SELECT name FROM sqlite_master WHERE type='table' and name='accesslogs';`);
+
+let row = stmt.get();
+if (row === undefined) {
+    console.log("Log database appears to be empty. Creating log database.");
+    const sqInit = `
+        CREATE TABLE accesslog ( 
+            id INTEGER PRIMARY KEY, 
+            remoteaddr TEXT,
+            remoteuser TEXT,
+            time TEXT,
+            method TEXT,
+            url TEXT,
+            protocol TEXT,
+            httpversion TEXT,
+            status TEXT, 
+            referrer TEXT,
+            useragent TEXT
+        );
+    `;
+} else {
+    console.log();
+}
+    
+   
