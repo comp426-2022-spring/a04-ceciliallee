43f59e0bca3f6369724dabe861a18051e3fd2905
Cecilia (Chae Hyun) Lee <chaelee@email.unc.edu>
2022-04-08 09:53:59 -0400

    ??

--- server.js
+++ server.js
@@ -1,42 +1,85 @@
-// const require = createRequire(import.meta.url);
-const express = require("express");
-const app = express();
-// const minimist = require("minimist");
+const args = require("minimist")(process.argv.slice(2));
+// See what is stored in the object produced by minimist
+//console.log('Command line arguments: ', args)
+// Store help text
+const help = `
+server.js [options]
+--port, -p	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+--debug, -d If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+--help, -h	Return this message and exit.
+`;
+// If --help, echo help text and exit
+if (args.help || args.h) {
+  console.log(help);
+  process.exit(0);
+}
+// Define app using express
+var express = require("express");
+var app = express();
+// Require fs
+const fs = require("fs");
+// Require morgan
 const morgan = require("morgan");
-
-const args = require('minimist')(process.argv.slice(2));
-const fs = require('fs')
-// const port = args["port"]
-const db = require("./database.js")
+// Require database SCRIPT file
+const db = require("./database.js");
+// Make Express use its own built-in body parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Server port
 const port = args.port || args.p || 5000;
-
-const help = (`server.js [options]
-    --port	Set the port number for the server to listen on. Must be an integer
-                between 1 and 65535.
-  
-    --debug	If set to \`true\`, creates endpoints /app/log/access/ which returns
-                a JSON access log from the database and /app/error which throws 
-                an error with the message "Error test successful." Defaults to 
-                \`false\`.
-  
-    --log	If set to false, no log files are written. Defaults to true.
-                Logs are always written to database.
-  
-    --help	Return this message and exit.`
-
-)
-  
-if (args.help || args.h) {
-  console.log(help)
-  process.exit(0)
-}
-
+// Start server
 const server = app.listen(port, () => {
-  console.log("App listening on port %PORT%".replace("%PORT%", port));
+  console.log("Server running on port %PORT%".replace("%PORT%", port));
 });
-
+// If --log=false then do not create a log file
+if (args.log == "false") {
+  console.log("NOTICE: not creating file access.log");
+} else {
+  // Use morgan for logging to files
+  // Create a write stream to append to an access.log file
+  const accessLog = fs.createWriteStream("access.log", { flags: "a" });
+  // Set up the access logging middleware
+  app.use(morgan("combined", { stream: accessLog }));
+}
+// Always log to database
+app.use((req, res, next) => {
+  let logdata = {
+    remoteaddr: req.ip,
+    remoteuser: req.user,
+    time: Date.now(),
+    method: req.method,
+    url: req.url,
+    protocol: req.protocol,
+    httpversion: req.httpVersion,
+    status: res.statusCode,
+    referrer: req.headers["referer"],
+    useragent: req.headers["user-agent"],
+  };
+  console.log(logdata);
+  const stmt = db.prepare(
+    "INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referrer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+  );
+  const info = stmt.run(
+    logdata.remoteaddr,
+    logdata.remoteuser,
+    logdata.time,
+    logdata.method,
+    logdata.url,
+    logdata.protocol,
+    logdata.httpversion,
+    logdata.status,
+    logdata.referrer,
+    logdata.useragent
+  );
+  //console.log(info)
+  next();
+});
 // coin functions
 function coinFlip() {
   let randomNum = Math.random();
@@ -93,33 +136,6 @@ function flipACoin(call) {
 }
 // end coin functions
 
-if (args.log == "false") {
-  console.log("NOTICE: not creating file access.log");
-} else {
-  const accessLog = fs.createWriteStream("access.log", { flags: "a" });
-  app.use(morgan("combined", { stream: accessLog }));
-}
-
-app.use((req, res, next) => {
-  let logData = {
-    remoteaddr: req.ip,
-    remoteuser: req.user,
-    time: Date.now(),
-    method: req.method,
-    url: req.url,
-    protocol: req.protocol,
-    httpversion: req.httpVersion,
-    secure: req.secure,
-    status: res.statusCode,
-    referer: req.headers["referer"],
-    useragent: req.headers["user-agent"],
-  };
-  console.log(logData);
-  const stmt = db.prepare(
-    "INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referrer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-  );
-  next();
-});
 
 app.get("/app/", (req, res) => {
   res.statusCode = 200;
